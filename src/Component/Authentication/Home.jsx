ff870079969c67293cdc9b98065d7e7b9df069b8
DUCNTHE151031 <ducnthe151031@fpt.edu.vn>
2025-08-17 23:01:31 +0700

    import home.css

--- src/Component/Authentication/Home.jsx
+++ src/Component/Authentication/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext.jsx';
 import { useNavigate } from 'react-router-dom';
+import '../../css/Home.css';
 import {
     Search,
     Lock,
