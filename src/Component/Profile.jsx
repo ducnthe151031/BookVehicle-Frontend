cc7d55883e18cec21a9b571450eb3818e37f8278
DUCNTHE151031 <ducnthe151031@fpt.edu.vn>
2025-09-12 19:07:16 +0700

    change update input name user length < 50 anh just input A-Za-z and space

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -391,7 +391,8 @@ const Profile = () => {
                                                     if (regex.test(value)) {
                                                         handleChange(e); // chỉ gọi khi hợp lệ
                                                     }
-                                                }}
+                                                }
+                                                }
                                                 className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                             />
                                         </div>
